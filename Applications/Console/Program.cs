73031f558c869a6fefd2288bb2d584e1b0d1d850
Abdurashid <Cabsho123@gmail.com>
2024-03-26 18:27:11 +0300

    Added Doubly LinkedList

--- Applications/Console/Program.cs
+++ Applications/Console/Program.cs
@@ -1,100 +1,58 @@
-﻿using DataStructures.Array;
+﻿using DataStructures.LinkedList.Doubly;
 using DataStructures.LinkedList.Singly;
 
-var linkedList = new SinglyLinkedList<string>();
-linkedList.AddLast("Omer");
-linkedList.AddLast("Osman");
-linkedList.AddLast("Harun");
-linkedList.AddLast("Kayra");
-linkedList.AddLast("Caner");
+var linkedlist = new DoublyLinkedList<char>("Samsun".ToArray());
+foreach (var item in linkedlist)
+{
+    System.Console.WriteLine(item);
+}
 
-// System.Console.WriteLine(linkedList.Head);
-// System.Console.WriteLine(linkedList.Head.Next);
-// System.Console.WriteLine(linkedList.Head.Next.Next);
-// System.Console.WriteLine(linkedList.Head.Next.Next.Next);
-// System.Console.WriteLine(linkedList.Head.Next.Next.Next.Next);
+Console.ReadKey();
 
-// var c = linkedList.Head;
+static void LinkedListTraverseSample()
+{
+    var linkedList = new SinglyLinkedList<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+    var curr = linkedList.GetEnumerator();
 
-// while (c != null)
-// {
-//     System.Console.WriteLine(c);
-//     c = c.Next;
-// }
+    while (curr.MoveNext())
+    {
+        System.Console.WriteLine(curr.Current);
+    }
+}
 
-// foreach (var item in linkedList)
-// {
-//     if (item.Contains("a"))
-//     {
-//         System.Console.WriteLine(item);
-//     }
-// }
-// var node = new SinglyLinkedListNode<string>("Omer");
-
-// var node1 = new SinglyLinkedListNode<string>();
-// node1.Value = "Enes";
-// node.Next = node1;
-
-// var node2 = new SinglyLinkedListNode<string>();
-// node2.Value = "Emre";
-// node1.Next = node2;
-
-// var node3 = new SinglyLinkedListNode<string>();
-// node3.Value = "Gülsüm";
-// node2.Next = node3;
-
-// var node4 = new SinglyLinkedListNode<string>();
-// node4.Value = "Peri";
-// node3.Next = node4;
+static void DdNodeSample()
+{
+    var hafsa = new DbNode<string>("hafsa");
+    var samet = new DbNode<string>("samet");
+    var emircan = new DbNode<string>("emircan");
+    var bahar = new DbNode<string>("bahar");
+    var zehra = new DbNode<string>("zehra");
+    var ismail = new DbNode<string>("ismail");
 
 
-// var current = node.Next;
+    hafsa.Next = zehra;
+    zehra.Next = samet;
+    zehra.Prev = hafsa;
+    samet.Next = ismail;
+    samet.Prev = zehra;
+    ismail.Next = new DbNode<string>("muhammed");
+    ismail.Prev = samet;
+    ismail.Next.Next = emircan;
+    ismail.Next.Prev = ismail;
+    emircan.Next = bahar;
+    emircan.Prev = ismail.Next;
+    bahar.Prev = emircan;
 
-// while (current != null)
-// {
-//     System.Console.WriteLine(current.Value);
-//     current = current.Next;
-// }
+    var head = hafsa;
+    var tail = bahar;
 
+    System.Console.WriteLine(head.Next == zehra); //hafsa,samet,true
+    System.Console.WriteLine(head.Next.Next); //samet
+    System.Console.WriteLine(head.Next.Next.Value); //samet
+    System.Console.WriteLine(tail); //bahar
+    System.Console.WriteLine(tail.Value); //bahar
+    System.Console.WriteLine(tail.Prev.Prev.Prev); //emircan,ismail
+}
 
-
-// public class Program
-// {
-//     public static void Main()
-//     {
-//         var array = new DataStructures.Array.Array<int>();
-//         array.Add(1);
-//         array.Add(2);
-//         array.Add(3);
-//         array.Add(4);
-//         array.Add(5);
-//         array.Add(6);
-
-//         array.RemoveAt(2);
-
-//         foreach (var item in array)
-//         {
-//             System.Console.WriteLine($"Item: {item,5} {array.Count,5}  {array.Capacity}");
-//         }
-
-//         StaticArray<string>();
-//     }
-
-//     static void StaticArray<T>()
-//     {
-//         var names = new DataStructures.Array.StaticArray<string>();
-
-//         names.SetItem(0, "Enes");
-//         names.SetItem(1, "Emre");
-//         names.SetItem(2, "Gülsüm");
-//         names.SetItem(3, "Peri");
-//         // names.SetItem(4,"Emircan");
-
-//         for (int i = 0; i < names.Length; i++)
-//         {
-//             System.Console.WriteLine(names.GetItem(i));
-//         }
-
-//         Console.ReadLine();
-//     }
-// }
+// DdNodeSample();
+// LinkedListTraverseSample();
