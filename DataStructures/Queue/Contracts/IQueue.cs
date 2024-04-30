b1d075f96feb07b4c608009f07b497181bdbcddc
Abdurashid <Cabsho123@gmail.com>
2024-05-01 00:15:36 +0300

    Queue has been created.

---
+++ DataStructures/Queue/Contracts/IQueue.cs
@@ -0,0 +1,11 @@
+namespace Datastructures.Queue.Contracts
+{
+    public interface IQueue<T>
+    {
+        public int Count { get; }
+        public void Enqueue(T item);
+        public T Dequeue();
+
+        public T Peek();
+    }
+}
